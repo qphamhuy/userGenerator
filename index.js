182282d73070a417c35aae3a4b67df5b71531d26
Quang Pham Huy <Quang Pham Huy>
2017-05-10 16:07:28 -0400

    Make names more pronounceable
    Implemented by forbidding certain chars to follow other chars

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var firstNames = require('./data/firstNames.json');
-var generateLastName = require('./lastNameGenerator.js');
+var lastNameGenerator = require('./lastNameGenerator.js');
 
-var numUsers = parseInt(process.argv[2] || 1);
+//Command-line arguments
+var numUsers = _getArgNum('n', 1);
+var minSyllables = _getArgNum('m', 1);
+var maxSyllables = _getArgNum('M', 3);
 
 for (var i = 0; i < numUsers; ++i) {
-    var fullName = firstNames[Math.floor(Math.random() * firstNames.length)] + ' ' + generateLastName(2);
-    console.log(fullName);
+    var firstName = firstNames[Math.floor(Math.random() * firstNames.length)].name;
+    var lastName = lastNameGenerator.generateLastName(minSyllables, maxSyllables);
+    console.log(firstName + ' ' + lastName);
+}
+
+/**
+ * Get number argument passed to command line after option -<char>
+ */
+function _getArgNum(c, defaultVal) {
+    var arg = process.argv.indexOf('-' + c);
+    if (arg === -1) return 1;
+    var num = parseInt(process.argv[arg + 1]);
+    return (isNaN(num) ? defaultVal : num);
 }
--- lastNameGenerator.js
+++ lastNameGenerator.js
@@ -3,37 +3,96 @@ var consonantsClusters_onset = require('./data/consonantClusters_onset.json');
 var consonantsClusters_coda = require('./data/consonantClusters_coda.json');
 var vowels = require('./data/vowels.json');
 
+module.exports = {
+    generateLastName: generateLastName
+};
+
+
+//////////
+
+
+/**
+ * Generate a random last name
+ * @param {Number} minSyllables - Min number of syllables in generated name
+ * @param {Number} maxSyllables - Max number of syllables in generated name
+ * @return {String} Random name
+ */
+function generateLastName(minSyllables, maxSyllables) {
+    var syllables = Math.floor(Math.random() * (maxSyllables - minSyllables)) + minSyllables;
+
+    var emptyChar = { "char": "", "invalidNext": [] };
+    var chars = [];
+
+    //First syllable
+    var startingConsonant = _randomElem(consonants.concat(consonantsClusters_onset, [emptyChar]))
+    chars.push(startingConsonant);
+    _addNextChars(chars, vowels);
+
+    for (var i = 0; i < syllables - 1; ++i) {
+        _addNextChars(chars, consonants);
+        _addNextChars(chars, vowels);
+    }
+
+    //Last syllable
+    var endingConsonants = consonants.concat(consonantsClusters_coda);
+    if (startingConsonant.char !== '' || syllables > 1)
+        endingConsonants.push(emptyChar);
+    _addNextChars(chars, endingConsonants);
+
+    return chars.map((c) => (c.char)).join('');
+}
+
+
+/**
+ * [private] Check if candidate next characters can follow current character
+ * @param {Object} curr - Current character object
+ * @param {Object} next - 1 or more next character objects
+ * @return {Boolean} True if given next characters can follow current character
+ */
+function _checkNextCharsValidity(curr, next) {
+    var nextCharsStr = next.map((c) => (c.char)).join('');
+    for (var i = 0; i < curr.invalidNext.length; ++i) {
+        if (new RegExp(curr.invalidNext[i]).test(nextCharsStr))
+            return false;
+    }
+    return true;
+}
+
+/**
+ * [private] Add next character(s) to accumulated array of name characters
+ * @param {Array} chars - Array of name characters
+ * @param {Array} charSet - Set of characters to choose next characters from
+ */
+function _addNextChars(chars, charSet) {
+    if (charSet === vowels) {
+        var nextChars = [];
+        while (true) {
+            nextChars = [_randomElem(charSet)];
+            if (Math.random() > 0.5) {
+                var nextVowel = _randomElem(charSet);
+                while (!_checkNextCharsValidity(nextChars[0], [nextVowel]))
+                    nextVowel = _randomElem(charSet);
+                nextChars.push(nextVowel);
+            }
+            if (_checkNextCharsValidity(chars[chars.length - 1], nextChars)) {
+                chars.push.apply(chars, nextChars);
+                break;
+            }
+        }
+    }
+    else {
+        var consonant = _randomElem(charSet);
+        while (!_checkNextCharsValidity(chars[chars.length - 1], [consonant]))
+            consonant = _randomElem(charSet);
+        chars.push(consonant);
+    }
+}
+
+/**
+ * [private] Get random element from an array
+ * @param {Array} arr - Array to choose from
+ * @return {Object} A random array element
+ */
 function _randomElem(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
-
-function _randomVowelGroup() {
-    var vowel = _randomElem(vowels);
-    return vowel.char + (Math.random() > 0.5 ? _randomElem(vowel.followers) : '');
-}
-
-function generateLastName(maxSyllables) {
-    // console.log(consonants.length + ", " + consonantsClusters_onset.length + ", " + consonantsClusters_coda.length);
-
-    var syllables = Math.floor(Math.random() * maxSyllables) + 1; //1-3 syllables
-
-    //Starting consonant
-    var startingConsonant = _randomElem(consonants.concat(consonantsClusters_onset).concat(['']));
-    var name = startingConsonant + _randomVowelGroup();
-
-    var i;
-    for (i = 0; i < syllables - 1; ++i) {
-        name += _randomElem(consonants);
-        name += _randomVowelGroup();
-    }
-
-    //Ending consonant
-    var endingConsonants = consonants.concat(consonantsClusters_coda);
-    if (startingConsonant !== '' || syllables > 1)
-        endingConsonants.push('');
-    name += _randomElem(endingConsonants);
-
-    return name[0].toUpperCase() + name.slice(1);
-}
-
-module.exports = generateLastName;
